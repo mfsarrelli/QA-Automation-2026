100e4f86a6f07d81f280115f4c4a20acd489f8fd
Maria Fernanda Sarrelli <mfsarrelli@gmail.com>
2026-04-20 20:02:02 -0300

    agrego markers y parametrize

--- test/test_calculadora.py
+++ test/test_calculadora.py
@@ -1,9 +1,20 @@
 import pytest
 from calculadora import sumar, dividir, restar, multiplicar
 
+@pytest.mark.suma
 def test_sumar_positivo():
-    assert sumar(2, 3) == 5
+#    assert sumar(2, 3) == 5
 
-def test_dividir_por_cero():
-    with pytest.raises(ValueError):
-        dividir(1, 0)
+##def test_dividir_por_cero():
+##    with pytest.raises(ValueError):
+##        dividir(1, 0)
+
+@pytest.mark.parametrize("a,b,resultado",[
+    (3,3,6),
+    (5,15,20),
+    (6,6,12)
+],
+ids=["caso1", "caso2","caso3"])
+
+def sumar_casos_parametrize(a,b,resultado):
+    assert sumar(a,b) == resultado
