dccd11937771de437ad3bd162dfdc1ed77cc8794
Maria Fernanda Sarrelli <mfsarrelli@gmail.com>
2026-05-02 12:42:33 -0300

    completo unidad 4 con ejercicio practico, incluye reporte html

--- test/test_calculadora.py
+++ test/test_calculadora.py
@@ -3,7 +3,11 @@ from calculadora import sumar, dividir, restar, multiplicar
 
 @pytest.mark.suma
 def test_sumar_positivo():
-#    assert sumar(2, 3) == 5
+    assert sumar(2, 3) == 5
+
+@pytest.mark.suma
+def test_sumar_negativo():
+    assert sumar(-10,-10) == -20
 
 ##def test_dividir_por_cero():
 ##    with pytest.raises(ValueError):
@@ -16,5 +20,5 @@ def test_sumar_positivo():
 ],
 ids=["caso1", "caso2","caso3"])
 
-def sumar_casos_parametrize(a,b,resultado):
+def test_sumar_casos_parametrize(a,b,resultado):
     assert sumar(a,b) == resultado
